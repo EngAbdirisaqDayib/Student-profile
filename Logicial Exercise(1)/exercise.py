5baa420acac93499ac34d74f02ec069201b42392
Eng Abdirisaq Dayib <abdirisaqdayibabdi@gmail.com>
2026-05-03 08:30:49 +0300

    Add files via upload

---
+++ Logicial Exercise(1)/exercise.py
@@ -0,0 +1,89 @@
+#Q1
+
+# x = 5
+# y = "5"
+# print (x + int(y) * 2)
+
+# output is 16 
+# int(y) , "5">5 , 5 * 2 =  10 , x + 10 , 6 +10 = 16
+
+# #Q2
+# x = 10
+# y = x
+# x = x + 5
+# y = y * 2
+# print(x, y)
+
+# #y = x copies the value that moment (10)
+
+
+# #Q3
+# data = ("python" , 3, 8.5)
+# a , b , c = data
+# print (a * b)
+# # pythonpythonpython
+# # multiplying string by number to repeat
+
+# #Q4
+# x = 10
+# y = "20"
+# print( x + y)
+
+#Fix 
+# x = 10
+# y = "20"
+# print(x + int(y))
+
+#Q6
+# print ("Age:" , 20 + 5, "Years" + "Old")
+#Without syntax Error
+# print ("Age:" , 20 + 5, "Years Old")
+
+#Q8
+# age = 25
+# Age = 30
+# print(age  + Age)
+
+#Output 55
+# Case-Sensitive
+
+#Q9
+# fruits = ("Apple", "Banana")
+# x , y , z = fruits
+# print(x)
+# print(y)
+
+#Value Error
+#fix fruits = ("Apple", "Banana" Orange)
+
+#Q10
+# x = "3"
+# y = 2
+# z = float(x) * y + int(x)
+# print(z)
+#Output is 9.0
+
+#Q11
+# print("hello")
+# print("world")
+# print("Python")
+
+#Outpu is hello_world_Python
+
+#Q12
+# x= "10" 
+# y = "5" 
+# print(int(x) + int(y) * int(x)) 
+# x = "10", int(x) = 10   #y = 5 int (y) = 5 
+#print (int("10") + int(5) * int("10")) output 60
+
+#Q13
+x = 5 
+x = x + 2 #10
+x = x * x # 
+x = x - 3 
+print(x) 
+
+ 
+ 
+
